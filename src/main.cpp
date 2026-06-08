444982a8536ba72b127c70aa1aa8b655aaebb26b
Nishantsensei <nishantpanwaraug21@gmail.com>
2026-06-08 15:25:43 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,15 @@ int main() {
     else if(command.substr(0,5) =="echo "){
       std::cout<<command.substr(5)<<'\n';
     }
+    else if(command.substr(0,5)=="type "){
+      if(command.substr(5)=="echo" || command.substr(5)=="exit" || command.substr(5)=="type" ){
+        std::cout<<command.substr(5)<<" is a shell builtin"<< std::endl;
+      }else{
+        std::cout<< command.substr(5)<< ": command not found" <<std::endl;
+      }
+    }
     else{
       std::cout<< command << ": command not found" <<std::endl;
     }
-    
   }
 }

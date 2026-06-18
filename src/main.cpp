d53d74c849c9419932533d92618d0d7468ebb788
Nishantsensei <nishantpanwaraug21@gmail.com>
2026-06-18 15:08:31 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-bool isBuiltin(const std::string& cmd){
+bool isBuiltin(const std::string& cmd)
+{
   std::vector<std::string> builtins ={"echo","exit","type","pwd","cd"};
-  for(const auto&b : builtins){
+  for(const auto&b : builtins)
+  {
     if(cmd == b) return true;
   }
   return false;
 }
 
-std::string findInPath(const std::string& cmd){
+std::string findInPath(const std::string& cmd)
+{
   const char* pathEnv = std::getenv("PATH");
   if(!pathEnv) return "";
 
@@ -24,12 +27,14 @@ std::string findInPath(const std::string& cmd){
   std::stringstream ss(pathStr);
   std::string dir;
 
-  while(std::getline(ss,dir,':')){
+  while(std::getline(ss,dir,':'))
+  {
     std::string fullPath =dir + "/"+cmd;
 
     std::ifstream file(fullPath);
     if(file.good()){
-      if(access(fullPath.c_str(), X_OK)==0){
+      if(access(fullPath.c_str(), X_OK)==0)
+      {
         return fullPath;
       }
     }
@@ -37,14 +42,21 @@ std::string findInPath(const std::string& cmd){
   return "";
 }
 
-void handleType(const std::string& cmd){
-  if(isBuiltin(cmd)){
+void handleType(const std::string& cmd)
+{
+  if(isBuiltin(cmd))
+  {
     std::cout <<cmd<< " is a shell builtin\n";
-  }else{
+  }
+  else
+  {
     std::string path=findInPath(cmd);
-    if(!path.empty()){
+    if(!path.empty())
+    {
       std::cout << cmd <<" is " << path<<"\n";
-    }else{
+    }
+    else
+    {
       std::cout <<cmd<< ": not found\n";
     }
   }
@@ -60,7 +72,8 @@ void executeExternal(const std::string& executable,
 
         std::vector<char*> argv;
 
-        for (const auto& arg : args) {
+        for (const auto& arg : args) 
+        {
             argv.push_back(const_cast<char*>(arg.c_str()));
         }
 
@@ -72,21 +85,24 @@ void executeExternal(const std::string& executable,
         perror("execvp");
         exit(1);
     }
-    else if (pid > 0) {
+    else if (pid > 0) 
+    {
         // Parent process waits
         waitpid(pid, nullptr, 0);
     }
-    else {
+    else 
+    {
         perror("fork");
     }
 }
 
 
-int main() {
-
+int main() 
+{
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  while(true){
+  while(true)
+  {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin,input);
@@ -100,36 +116,59 @@ int main() {
     if(args.empty())continue;
     std::string command = args[0];
 
-    if(command=="exit"){ 
+    if(command=="exit")
+    { 
       break;
     }
-    else if(command =="echo"){
-       for(int i = 1; i < args.size(); i++){
+    else if(command =="echo")
+    {
+       for(int i = 1; i < args.size(); i++)
+       {
         std::cout << args[i];
         if(i < args.size() - 1) std::cout << " "; 
       }
       std::cout << "\n";
     }
-    else if(command=="type"){
-      if (args.size() > 1) {
+    else if(command=="type")
+    {
+      if (args.size() > 1) 
+      {
         handleType(args[1]);  
       }
     }
-    else if(command == "pwd"){
+    else if(command == "pwd")
+    {
       char cwd[1024];
 
-      if(getcwd(cwd, sizeof(cwd)) != nullptr){
-          std::cout << cwd << "\n";
+      if(getcwd(cwd, sizeof(cwd)) != nullptr)
+      {
+        std::cout << cwd << "\n";
       }
     }
+    else if(command == "cd")
+    {
+      if(args.size()>2)
+      {
+        continue;
+      }
+      if(chdir(args[1].c_str())!=0)
+      {
+        std::cout<< "cd: "<< args[1] << ": No such file or directory"<< std::endl;
+      }
+      
+    }
     
-    else {
+    else 
+    {
     std::string executable = findInPath(command);
 
-      if (!executable.empty()) {
-          executeExternal(executable, args);
-      } else {
-          std::cout << command<< ": command not found"<< std::endl;
+      if (!executable.empty()) 
+      {
+        executeExternal(executable, args);
+      }
+      else 
+      {
+        std::cout << command<< ": command not found"<< std::endl;
       }
     }
   }
